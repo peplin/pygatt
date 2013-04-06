58947a7c4ad08917538a1312566f8df0103c6ca9
Michael Saunby <mike@saunby.net>
2013-04-06 18:08:07 +0100

    simple example, tested on Ubuntu 12.10 and Raspbian wheezy

--- README.md
+++ README.md
@@ -2,3 +2,19 @@ ble-sensor-pi
 =============
 
 Simple example for SensorTag with RaspberryPi a Bluetooth Low Engery (BLE GATT) device
+
+See sensortag/sensortag_test.py for a simple example of reading temperature values from the tag.
+The same approach could be used to read any of the sensors.  It's also possible to have the tag
+send a stream of measurements at regular intervals.  See the information here -
+http://processors.wiki.ti.com/index.php/SensorTag_User_Guide
+Or wait for me to add more examples.
+
+This code was thrown together quickly to see how easy it might be to use these devices with the
+RaspberryPi.  It proved to be very easy - though I struggled to find much information on BLE on 
+Linux.  Other relevant search terms are GATT.
+
+I'll post more information on my blog at http://mike.saunby.net
+
+Note you might need to install a newer version of gatttool for this code to work.  I used the 
+version included in bluez-5.2 - it's in the attrib directory.
+
---
+++ sensortag/sensortag_test.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# Michael Saunby. April 2013   
+# 
+# Read temperature from the TMP006 sensor in the TI SensorTag 
+# It's a BLE (Bluetooth low energy) device so using gatttool to
+# read and write values. 
+#
+# Usage.
+# sensortag_test.py BLUETOOTH_ADR
+#
+# To find the address of your SensorTag run 'sudo hcitool lescan'
+# You'll need to press the side button to enable discovery.
+#
+# Notes.
+# pexpect uses regular expression so characters that have special meaning
+# in regular expressions, e.g. [ and ] must be escaped with a backslash.
+#
+
+import pexpect
+import sys
+import time
+
+def floatfromhex(h):
+    t = float.fromhex(h)
+    if t > float.fromhex('7FFF'):
+        t = -(float.fromhex('FFFF') - t)
+        pass
+    return t
+
+
+# This algorithm borrowed from 
+# http://processors.wiki.ti.com/index.php/SensorTag_User_Guide#Gatt_Server
+# which most likely took it from the datasheet.  I've not checked it, other
+# than noted that the temperatue values I got seemed reasonable.
+#
+def calcTmpTarget(objT, ambT):
+    m_tmpAmb = ambT/128.0
+    Vobj2 = objT * 0.00000015625
+    Tdie2 = m_tmpAmb + 273.15
+    S0 = 6.4E-14            # Calibration factor
+    a1 = 1.75E-3
+    a2 = -1.678E-5
+    b0 = -2.94E-5
+    b1 = -5.7E-7
+    b2 = 4.63E-9
+    c2 = 13.4
+    Tref = 298.15
+    S = S0*(1+a1*(Tdie2 - Tref)+a2*pow((Tdie2 - Tref),2))
+    Vos = b0 + b1*(Tdie2 - Tref) + b2*pow((Tdie2 - Tref),2)
+    fObj = (Vobj2 - Vos) + c2*pow((Vobj2 - Vos),2)
+    tObj = pow(pow(Tdie2,4) + (fObj/S),.25)
+    tObj = (tObj - 273.15)
+    print "%.2f C" % tObj
+
+
+bluetooth_adr = sys.argv[1]
+tool = pexpect.spawn('gatttool -b ' + bluetooth_adr + ' --interactive')
+tool.expect('\[LE\]>')
+print "Preparing to connect. You might need to press the side button..."
+tool.sendline('connect')
+# test for success of connect
+tool.expect('\[CON\].*>')
+tool.sendline('char-write-cmd 0x29 01')
+tool.expect('\[LE\]>')
+while True:
+    time.sleep(1)
+    tool.sendline('char-read-hnd 0x25')
+    tool.expect('descriptor: .*') 
+    rval = tool.after.split()
+    objT = floatfromhex(rval[2] + rval[1])
+    ambT = floatfromhex(rval[4] + rval[3])
+    #print rval
+    calcTmpTarget(objT, ambT)
+
+
