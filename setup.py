c91971da30ab646105e1187126104b361df00e5b
Greg Albrecht <gba@onbeep.com>
2014-12-14 02:07:30 -0800

    bumped to 1.0.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ Source:: https://github.com/ampledata/aprs
 
 
 __title__ = 'pygatt'
-__version__ = '0.1.0'
+__version__ = '1.0.0'
 __author__ = 'Greg Albrecht <gba@onbeep.com>'
 __license__ = 'Apache License, Version 2.0'
 __copyright__ = 'Copyright 2014 OnBeep, Inc.'
