d0857f93ad650bb9d18e74f267eadc069b041e46
Christopher Peplin <chris.peplin@rhubarbtech.com>
2015-07-30 18:55:19 -0400

    Pull up common code from tests into a class-level tearDown.

--- tests/pygatt_tests.py
+++ tests/pygatt_tests.py
@@ -72,6 +72,8 @@ class BGAPIBackendTests(unittest.TestCase):
                                     run=False)
 
     def tearDown(self):
+        self.backend.stop()
+
         for patcher in self.patchers:
             try:
                 patcher.stop()
@@ -416,211 +418,169 @@ class BGAPIBackendTests(unittest.TestCase):
                 connection_handle, handle, 0x00,
                 value=[len(val)+1]+val))
 
-    def test_create_backend(self):
-        """__init__ general functionality."""
-        backend = None
-        backend = BGAPIBackend(
-            serial_port='dummy', logfile=self.null_file, run=False)
-        # TODO this isn't testing anything - the constructor will always return
-        # something unless it raised an exception, and if it raised an exception
-        # this assertion line will never get called
-        assert(backend is not None)
-
     def test_run_backend(self):
         """run general functionality."""
-        try:
-            # Test run
-            self._stage_run_packets(self.backend)
-            self.backend.run()
-        finally:
-            # TODO This is in every test - is it necessary? can it be moved to
-            # tearDown?
-            self.backend.stop()
+        self._stage_run_packets(self.backend)
+        self.backend.run()
 
     def test_connect(self):
         """connect general functionality."""
-        try:
-            self._stage_run_packets(self.backend)
-            self.backend.run()
-            # Test connect
-            address = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB]
-            self._stage_connect_packets(
-                self.backend, address, ['connected', 'completed'])
-            self.backend.connect(bytearray(address))
-        finally:
-            self.backend.stop()
+        self._stage_run_packets(self.backend)
+        self.backend.run()
+        # Test connect
+        address = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB]
+        self._stage_connect_packets(
+            self.backend, address, ['connected', 'completed'])
+        self.backend.connect(bytearray(address))
 
     def test_disconnect_when_connected(self):
         """disconnect general functionality."""
-        try:
-            self._stage_run_packets(self.backend)
-            self.backend.run()
-            address = [0x01, 0x23, 0x45, 0x67, 0x89, 0xab]
-            self._stage_connect_packets(
-                self.backend, address, ['connected', 'completed'])
-            self.backend.connect(bytearray(address))
-            # test disconnect (connected, not fail)
-            self._stage_disconnect_packets(self.backend, True, False)
-            self.backend.disconnect()
-        finally:
-            self.backend.stop()
+        self._stage_run_packets(self.backend)
+        self.backend.run()
+        address = [0x01, 0x23, 0x45, 0x67, 0x89, 0xab]
+        self._stage_connect_packets(
+            self.backend, address, ['connected', 'completed'])
+        self.backend.connect(bytearray(address))
+        # test disconnect (connected, not fail)
+        self._stage_disconnect_packets(self.backend, True, False)
+        self.backend.disconnect()
 
     def test_char_read(self):
         """read general functionality."""
-        try:
-            self._stage_run_packets(self.backend)
-            self.backend.run()
-            address = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB]
-            self._stage_connect_packets(
-                self.backend, address, ['connected', 'completed'])
-            self.backend.connect(bytearray(address))
-            uuid_char = '01234567-0123-0123-0123-0123456789AB'
-            handle_char = 0x1234
-            uuid_desc = '2902'
-            handle_desc = 0x5678
-            self._stage_get_handle_packets(self.backend, [
-                uuid_char, handle_char,
-                uuid_desc, handle_desc])
-            handle = self.backend.get_handle(
-                self._uuid_str_to_bytearray(uuid_char))
-            # Test char_read
-            expected_value = [0xBE, 0xEF, 0x15, 0xF0, 0x0D]
-            self._stage_char_read_packets(
-                self.backend, handle, 0x00, expected_value)
-            value = self.backend.char_read(handle)
-            assert(value == bytearray(expected_value))
-        finally:
-            self.backend.stop()
+        self._stage_run_packets(self.backend)
+        self.backend.run()
+        address = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB]
+        self._stage_connect_packets(
+            self.backend, address, ['connected', 'completed'])
+        self.backend.connect(bytearray(address))
+        uuid_char = '01234567-0123-0123-0123-0123456789AB'
+        handle_char = 0x1234
+        uuid_desc = '2902'
+        handle_desc = 0x5678
+        self._stage_get_handle_packets(self.backend, [
+            uuid_char, handle_char,
+            uuid_desc, handle_desc])
+        handle = self.backend.get_handle(
+            self._uuid_str_to_bytearray(uuid_char))
+        # Test char_read
+        expected_value = [0xBE, 0xEF, 0x15, 0xF0, 0x0D]
+        self._stage_char_read_packets(
+            self.backend, handle, 0x00, expected_value)
+        value = self.backend.char_read(handle)
+        assert(value == bytearray(expected_value))
 
     def test_char_write(self):
         """char_write general functionality."""
-        try:
-            self._stage_run_packets(self.backend)
-            self.backend.run()
-            address = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB]
-            self._stage_connect_packets(
-                self.backend, address, ['connected', 'completed'])
-            self.backend.connect(bytearray(address))
-            uuid_char = '01234567-0123-0123-0123-0123456789AB'
-            handle_char = 0x1234
-            uuid_desc = '2902'
-            handle_desc = 0x5678
-            self._stage_get_handle_packets(self.backend, [
-                uuid_char, handle_char,
-                uuid_desc, handle_desc])
-            handle = self.backend.get_handle(
-                self._uuid_str_to_bytearray(uuid_char))
-            # Test char_write
-            value = [0xF0, 0x0F, 0x00]
-            self._stage_char_write_packets(self.backend, handle, value)
-            self.backend.char_write(handle, bytearray(value))
-        finally:
-            self.backend.stop()
+        self._stage_run_packets(self.backend)
+        self.backend.run()
+        address = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB]
+        self._stage_connect_packets(
+            self.backend, address, ['connected', 'completed'])
+        self.backend.connect(bytearray(address))
+        uuid_char = '01234567-0123-0123-0123-0123456789AB'
+        handle_char = 0x1234
+        uuid_desc = '2902'
+        handle_desc = 0x5678
+        self._stage_get_handle_packets(self.backend, [
+            uuid_char, handle_char,
+            uuid_desc, handle_desc])
+        handle = self.backend.get_handle(
+            self._uuid_str_to_bytearray(uuid_char))
+        # Test char_write
+        value = [0xF0, 0x0F, 0x00]
+        self._stage_char_write_packets(self.backend, handle, value)
+        self.backend.char_write(handle, bytearray(value))
 
     def test_encrypt(self):
         """encrypt general functionality."""
-        try:
-            self._stage_run_packets(self.backend)
-            self.backend.run()
-            address = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB]
-            self._stage_connect_packets(
-                self.backend, address, ['connected', 'completed'])
-            self.backend.connect(bytearray(address))
-            # Test encrypt
-            self._stage_encrypt_packets(
-                self.backend, address, ['connected', 'encrypted'])
-            self.backend.encrypt()
-        finally:
-            self.backend.stop()
+        self._stage_run_packets(self.backend)
+        self.backend.run()
+        address = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB]
+        self._stage_connect_packets(
+            self.backend, address, ['connected', 'completed'])
+        self.backend.connect(bytearray(address))
+        # Test encrypt
+        self._stage_encrypt_packets(
+            self.backend, address, ['connected', 'encrypted'])
+        self.backend.encrypt()
 
     def test_bond(self):
         """bond general functionality."""
-        try:
-            self._stage_run_packets(self.backend)
-            self.backend.run()
-            address = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB]
-            self._stage_connect_packets(
-                self.backend, address, ['connected', 'completed'])
-            self.backend.connect(bytearray(address))
-            self._stage_bond_packets(self.backend, address,
-                                     ['connected', 'encrypted',
-                                      'parameters_change'])
-            self.backend.bond()
-        finally:
-            self.backend.stop()
+        self._stage_run_packets(self.backend)
+        self.backend.run()
+        address = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB]
+        self._stage_connect_packets(
+            self.backend, address, ['connected', 'completed'])
+        self.backend.connect(bytearray(address))
+        self._stage_bond_packets(self.backend, address,
+                                 ['connected', 'encrypted',
+                                  'parameters_change'])
+        self.backend.bond()
 
     def test_get_rssi(self):
         """get_rssi general functionality."""
-        try:
-            self._stage_run_packets(self.backend)
-            self.backend.run()
-            address = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB]
-            self._stage_connect_packets(
-                self.backend, address, ['connected', 'completed'])
-            self.backend.connect(bytearray(address))
-            # Test get_rssi
-            self._stage_get_rssi_packets(self.backend)
-            assert(self.backend.get_rssi() == -80)
-        finally:
-            self.backend.stop()
+        self._stage_run_packets(self.backend)
+        self.backend.run()
+        address = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB]
+        self._stage_connect_packets(
+            self.backend, address, ['connected', 'completed'])
+        self.backend.connect(bytearray(address))
+        # Test get_rssi
+        self._stage_get_rssi_packets(self.backend)
+        assert(self.backend.get_rssi() == -80)
 
     def test_get_handle(self):
         """get_handle general functionality."""
-        try:
-            self._stage_run_packets(self.backend)
-            self.backend.run()
-            address = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB]
-            self._stage_connect_packets(
-                self.backend, address, ['connected', 'completed'])
-            self.backend.connect(bytearray(address))
-            # Test get_handle
-            uuid_char = '01234567-0123-0123-0123-0123456789AB'
-            handle_char = 0x1234
-            uuid_desc = '2902'
-            handle_desc = 0x5678
-            self._stage_get_handle_packets(self.backend, [
-                uuid_char, handle_char,
-                uuid_desc, handle_desc])
-            handle = self.backend.get_handle(
-                self._uuid_str_to_bytearray(uuid_char))
-            assert(handle == handle_char)
-            handle = self.backend.get_handle(
-                self._uuid_str_to_bytearray(uuid_char),
-                self._uuid_str_to_bytearray(uuid_desc))
-            assert(handle == handle_desc)
-        finally:
-            self.backend.stop()
+        self._stage_run_packets(self.backend)
+        self.backend.run()
+        address = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB]
+        self._stage_connect_packets(
+            self.backend, address, ['connected', 'completed'])
+        self.backend.connect(bytearray(address))
+        # Test get_handle
+        uuid_char = '01234567-0123-0123-0123-0123456789AB'
+        handle_char = 0x1234
+        uuid_desc = '2902'
+        handle_desc = 0x5678
+        self._stage_get_handle_packets(self.backend, [
+            uuid_char, handle_char,
+            uuid_desc, handle_desc])
+        handle = self.backend.get_handle(
+            self._uuid_str_to_bytearray(uuid_char))
+        assert(handle == handle_char)
+        handle = self.backend.get_handle(
+            self._uuid_str_to_bytearray(uuid_char),
+            self._uuid_str_to_bytearray(uuid_desc))
+        assert(handle == handle_desc)
 
     def test_scan_and_get_devices_discovered(self):
         """scan/get_devices_discovered general functionality."""
-        try:
-            self._stage_run_packets(self.backend)
-            self.backend.run()
-            # Test scan
-            scan_responses = []
-            addr_0 = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB]
-            addr_0_str = ':'.join('%02x' % b for b in addr_0)
-            scan_responses.append({
-                'rssi': -80,
-                'packet_type': 0,
-                'bd_addr': addr_0,
-                'addr_type': 0x00,
-                'bond': 0xFF,
-                'data': [0x07, 0x09, ord('H'), ord('e'), ord('l'),
-                         ord('l'), ord('o'), ord('!')]
-            })
-            self._stage_scan_packets(self.backend,
-                                     scan_responses=scan_responses)
-            self.backend.scan()
-            devs = self.backend.get_devices_discovered()
-            assert_in(addr_0_str, devs)
-            eq_('Hello!', devs[addr_0_str].name)
-            eq_(-80, devs[addr_0_str].rssi)
-        finally:
-            self.backend.stop()
+        self._stage_run_packets(self.backend)
+        self.backend.run()
+        # Test scan
+        scan_responses = []
+        addr_0 = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB]
+        addr_0_str = ':'.join('%02x' % b for b in addr_0)
+        scan_responses.append({
+            'rssi': -80,
+            'packet_type': 0,
+            'bd_addr': addr_0,
+            'addr_type': 0x00,
+            'bond': 0xFF,
+            'data': [0x07, 0x09, ord('H'), ord('e'), ord('l'),
+                     ord('l'), ord('o'), ord('!')]
+        })
+        self._stage_scan_packets(self.backend,
+                                 scan_responses=scan_responses)
+        self.backend.scan()
+        devs = self.backend.get_devices_discovered()
+        assert_in(addr_0_str, devs)
+        eq_('Hello!', devs[addr_0_str].name)
+        eq_(-80, devs[addr_0_str].rssi)
 
     def test_subscribe_with_notify(self):
         """subscribe with notify general functionality."""
+
         class NotificationHandler(object):
             def __init__(self, expected_value_bytearray):
                 self.expected_value_bytearray = expected_value_bytearray
@@ -631,55 +591,46 @@ class BGAPIBackendTests(unittest.TestCase):
                 self.received_value_bytearray = received_value_bytearray
                 self.called.set()
 
-        try:
-            self._stage_run_packets(self.backend)
-            self.backend.run()
-            address = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB]
-            self._stage_connect_packets(
-                self.backend, address, ['connected', 'completed'])
-            self.backend.connect(bytearray(address))
-            # Test subscribe with indications
-            packet_values = [bytearray([0xF0, 0x0D, 0xBE, 0xEF])]
-            my_handler = NotificationHandler(packet_values[0])
-            handle = 0x1234
-            uuid = '01234567-0123-0123-0123-0123456789AB'
-            self._stage_subscribe_packets(self.backend, uuid, handle)
-            self.backend.subscribe(self._uuid_str_to_bytearray(uuid),
-                                   callback=my_handler.handle, indicate=True)
-            start_time = time.time()
-            self._stage_indication_packets(self.backend, handle, packet_values)
-            while not my_handler.called.is_set():
-                elapsed_time = start_time - time.time()
-                if elapsed_time >= 5:
-                    raise Exception("Callback wasn't called after {0} seconds."
-                                    .format(elapsed_time))
-            print([b for b in my_handler.expected_value_bytearray])
-            print([b for b in my_handler.received_value_bytearray])
-            assert(my_handler.expected_value_bytearray ==
-                   my_handler.received_value_bytearray)
-        finally:
-            self.backend.stop()
+        self._stage_run_packets(self.backend)
+        self.backend.run()
+        address = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB]
+        self._stage_connect_packets(
+            self.backend, address, ['connected', 'completed'])
+        self.backend.connect(bytearray(address))
+        # Test subscribe with indications
+        packet_values = [bytearray([0xF0, 0x0D, 0xBE, 0xEF])]
+        my_handler = NotificationHandler(packet_values[0])
+        handle = 0x1234
+        uuid = '01234567-0123-0123-0123-0123456789AB'
+        self._stage_subscribe_packets(self.backend, uuid, handle)
+        self.backend.subscribe(self._uuid_str_to_bytearray(uuid),
+                               callback=my_handler.handle, indicate=True)
+        start_time = time.time()
+        self._stage_indication_packets(self.backend, handle, packet_values)
+        while not my_handler.called.is_set():
+            elapsed_time = start_time - time.time()
+            if elapsed_time >= 5:
+                raise Exception("Callback wasn't called after {0} seconds."
+                                .format(elapsed_time))
+        print([b for b in my_handler.expected_value_bytearray])
+        print([b for b in my_handler.received_value_bytearray])
+        assert(my_handler.expected_value_bytearray ==
+               my_handler.received_value_bytearray)
 
     def test_delete_stored_bonds(self):
         """delete_stored_bonds general functionality."""
-        try:
-            self._stage_run_packets(self.backend)
-            self.backend.run()
-            # Test delete stored bonds
-            self._stage_delete_stored_bonds_packets(
-                self.backend, [0x00, 0x01, 0x02, 0x03, 0x04])
-            self.backend.delete_stored_bonds()
-        finally:
-            self.backend.stop()
+        self._stage_run_packets(self.backend)
+        self.backend.run()
+        # Test delete stored bonds
+        self._stage_delete_stored_bonds_packets(
+            self.backend, [0x00, 0x01, 0x02, 0x03, 0x04])
+        self.backend.delete_stored_bonds()
 
     def test_delete_stored_bonds_disconnect(self):
         """delete_stored_bonds shouldn't abort if disconnected."""
-        try:
-            self._stage_run_packets(self.backend)
-            self.backend.run()
-            # Test delete stored bonds
-            self._stage_delete_stored_bonds_packets(
-                self.backend, [0x00, 0x01, 0x02, 0x03, 0x04], disconnects=True)
-            self.backend.delete_stored_bonds()
-        finally:
-            self.backend.stop()
+        self._stage_run_packets(self.backend)
+        self.backend.run()
+        # Test delete stored bonds
+        self._stage_delete_stored_bonds_packets(
+            self.backend, [0x00, 0x01, 0x02, 0x03, 0x04], disconnects=True)
+        self.backend.delete_stored_bonds()
