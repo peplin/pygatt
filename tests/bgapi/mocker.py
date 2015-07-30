ccf5446511bc878f922134f44eded8d7730195be
Christopher Peplin <chris.peplin@rhubarbtech.com>
2015-07-30 19:45:58 -0400

    Refactor BGAPI backend spy - it is simply a serial data mocker.

--- tests/bgapi/mocker.py
+++ tests/bgapi/mocker.py
@@ -1,10 +1,20 @@
+from mock import patch
+
+from tests.serial_mock import SerialMock
+
 from .packets import BGAPIPacketBuilder
 from .util import uuid_to_bytearray
 
 
-class BGAPIBackendSpy(object):
-    def __init__(self, backend):
-        self.backend = backend
+class MockBGAPISerialDevice(object):
+    def __init__(self, serial_port_name='mock'):
+        self.serial_port_name = serial_port_name
+        self.mocked_serial = SerialMock(self.serial_port_name, 0.25)
+        self.patcher = patch('serial.Serial',
+                             return_value=self.mocked_serial).start()
+
+    def stop(self):
+        self.patcher.stop()
 
     @staticmethod
     def _get_connection_status_flags_byte(flags):
@@ -22,28 +32,26 @@ class BGAPIBackendSpy(object):
     def stage_disconnected_by_remote(
             self, connection_handle=0x00):
         # Stage ble_evt_connection_disconnected (terminated by remote user)
-        self.backend._ser.stage_output(
+        self.mocked_serial.stage_output(
             BGAPIPacketBuilder.connection_disconnected(
                 connection_handle, 0x0213))
 
-    def stage_disconnect_packets(
-            self, connected, fail, connection_handle=0x00):
-        """Stage the packets for backend.disconnect()."""
+    def stage_disconnect_packets(self, connected, fail, connection_handle=0x00):
         if connected:
             if fail:
                 raise NotImplementedError()
 
             # Stage ble_rsp_connection_disconnect (success)
-            self.backend._ser.stage_output(
+            self.mocked_serial.stage_output(
                 BGAPIPacketBuilder.connection_disconnect(
                     connection_handle, 0x0000))
             # Stage ble_evt_connection_disconnected (success by local user)
-            self.backend._ser.stage_output(
+            self.mocked_serial.stage_output(
                 BGAPIPacketBuilder.connection_disconnected(
                     connection_handle, 0x0000))
         else:  # not connected always fails
             # Stage ble_rsp_connection_disconnect (fail, not connected)
-            self.backend._ser.stage_output(
+            self.mocked_serial.stage_output(
                 BGAPIPacketBuilder.connection_disconnect(
                     connection_handle, 0x0186))
 
@@ -51,58 +59,58 @@ class BGAPIBackendSpy(object):
         # Stage ble_rsp_connection_disconnect (not connected, fail)
         self.stage_disconnect_packets(False, True)
         # Stage ble_rsp_gap_set_mode (success)
-        self.backend._ser.stage_output(BGAPIPacketBuilder.gap_set_mode(0x0000))
+        self.mocked_serial.stage_output(BGAPIPacketBuilder.gap_set_mode(0x0000))
         # Stage ble_rsp_gap_end_procedure (fail, device in wrong state)
-        self.backend._ser.stage_output(
+        self.mocked_serial.stage_output(
             BGAPIPacketBuilder.gap_end_procedure(0x0181))
         # Stage ble_rsp_sm_set_bondable_mode (always success)
-        self.backend._ser.stage_output(
+        self.mocked_serial.stage_output(
             BGAPIPacketBuilder.sm_set_bondable_mode())
 
     def stage_connect_packets(self, addr, flags, connection_handle=0x00):
         # Stage ble_rsp_gap_connect_direct (success)
-        self.backend._ser.stage_output(
+        self.mocked_serial.stage_output(
             BGAPIPacketBuilder.gap_connect_direct(connection_handle, 0x0000))
         # Stage ble_evt_connection_status
         flags_byte = self._get_connection_status_flags_byte(flags)
-        self.backend._ser.stage_output(BGAPIPacketBuilder.connection_status(
+        self.mocked_serial.stage_output(BGAPIPacketBuilder.connection_status(
             addr, flags_byte, connection_handle, 0,
             0x0014, 0x0006, 0x0000, 0xFF))
 
     def stage_get_rssi_packets(self, connection_handle=0x00,
                                rssi=-80):
         # Stage ble_rsp_connection_get_rssi
-        self.backend._ser.stage_output(
+        self.mocked_serial.stage_output(
             BGAPIPacketBuilder.connection_get_rssi(connection_handle, rssi))
 
     def stage_encrypt_packets(self, addr, flags,
                               connection_handle=0x00):
         # Stage ble_rsp_sm_set_bondable_mode (always success)
-        self.backend._ser.stage_output(
+        self.mocked_serial.stage_output(
             BGAPIPacketBuilder.sm_set_bondable_mode())
         # Stage ble_rsp_sm_encrypt_start (success)
-        self.backend._ser.stage_output(BGAPIPacketBuilder.sm_encrypt_start(
+        self.mocked_serial.stage_output(BGAPIPacketBuilder.sm_encrypt_start(
             connection_handle, 0x0000))
         # Stage ble_evt_connection_status
         flags_byte = self._get_connection_status_flags_byte(flags)
-        self.backend._ser.stage_output(BGAPIPacketBuilder.connection_status(
+        self.mocked_serial.stage_output(BGAPIPacketBuilder.connection_status(
             addr, flags_byte, connection_handle, 0,
             0x0014, 0x0006, 0x0000, 0xFF))
 
     def stage_bond_packets(self, addr, flags,
                            connection_handle=0x00, bond_handle=0x01):
         # Stage ble_rsp_sm_set_bondable_mode (always success)
-        self.backend._ser.stage_output(
+        self.mocked_serial.stage_output(
             BGAPIPacketBuilder.sm_set_bondable_mode())
         # Stage ble_rsp_sm_encrypt_start (success)
-        self.backend._ser.stage_output(BGAPIPacketBuilder.sm_encrypt_start(
+        self.mocked_serial.stage_output(BGAPIPacketBuilder.sm_encrypt_start(
             connection_handle, 0x0000))
         # Stage ble_evt_sm_bond_status
-        self.backend._ser.stage_output(BGAPIPacketBuilder.sm_bond_status(
+        self.mocked_serial.stage_output(BGAPIPacketBuilder.sm_bond_status(
             bond_handle, 0x00, 0x00, 0x00))
         # Stage ble_evt_connection_status
         flags_byte = self._get_connection_status_flags_byte(flags)
-        self.backend._ser.stage_output(BGAPIPacketBuilder.connection_status(
+        self.mocked_serial.stage_output(BGAPIPacketBuilder.connection_status(
             addr, flags_byte, connection_handle, 0,
             0x0014, 0x0006, 0x0000, 0xFF))
 
@@ -112,42 +120,43 @@ class BGAPIBackendSpy(object):
         if disconnects:
             self.stage_disconnected_by_remote()
         # Stage ble_rsp_get_bonds
-        self.backend._ser.stage_output(
+        self.mocked_serial.stage_output(
             BGAPIPacketBuilder.sm_get_bonds(len(bonds)))
         # Stage ble_evt_sm_bond_status (bond handle)
         for b in bonds:
             if disconnects:
                 self.stage_disconnected_by_remote()
-            self.backend._ser.stage_output(BGAPIPacketBuilder.sm_bond_status(
+            self.mocked_serial.stage_output(BGAPIPacketBuilder.sm_bond_status(
                 b, 0x00, 0x00, 0x00))
         # Stage ble_rsp_sm_delete_bonding (success)
         for b in bonds:
             if disconnects:
                 self.stage_disconnected_by_remote()
-            self.backend._ser.stage_output(
+            self.mocked_serial.stage_output(
                 BGAPIPacketBuilder.sm_delete_bonding(0x0000))
 
     def stage_scan_packets(self, scan_responses=[]):
         # Stage ble_rsp_gap_set_scan_parameters (success)
-        self.backend._ser.stage_output(
+        self.mocked_serial.stage_output(
             BGAPIPacketBuilder.gap_set_scan_parameters(0x0000))
         # Stage ble_rsp_gap_discover (success)
-        self.backend._ser.stage_output(
+        self.mocked_serial.stage_output(
             BGAPIPacketBuilder.gap_discover(0x0000))
         for srp in scan_responses:
             # Stage ble_evt_gap_scan_response
-            self.backend._ser.stage_output(BGAPIPacketBuilder.gap_scan_response(
-                srp['rssi'], srp['packet_type'], srp['bd_addr'],
-                srp['addr_type'], srp['bond'],
-                [len(srp['data'])+1]+srp['data']))
+            self.mocked_serial.stage_output(
+                BGAPIPacketBuilder.gap_scan_response(
+                    srp['rssi'], srp['packet_type'], srp['bd_addr'],
+                    srp['addr_type'], srp['bond'],
+                    [len(srp['data'])+1]+srp['data']))
         # Stage ble_rsp_gap_end_procedure (success)
-        self.backend._ser.stage_output(
+        self.mocked_serial.stage_output(
             BGAPIPacketBuilder.gap_end_procedure(0x0000))
 
     def stage_get_handle_packets(
             self, uuid_handle_list, connection_handle=0x00):
         # Stage ble_rsp_attclient_find_information (success)
-        self.backend._ser.stage_output(
+        self.mocked_serial.stage_output(
             BGAPIPacketBuilder.attclient_find_information(
                 connection_handle, 0x0000))
         for i in range(0, len(uuid_handle_list)/2):
@@ -155,59 +164,42 @@ class BGAPIBackendSpy(object):
             handle = uuid_handle_list[2*i + 1]
             # Stage ble_evt_attclient_find_information_found
             u = [len(uuid) + 1]
-            self.backend._ser.stage_output(
+            self.mocked_serial.stage_output(
                 BGAPIPacketBuilder.attclient_find_information_found(
                     connection_handle, handle,
                     (u+list(reversed([ord(b) for b in uuid])))))
         # Stage ble_evt_attclient_procedure_completed (success)
-        self.backend._ser.stage_output(
+        self.mocked_serial.stage_output(
             BGAPIPacketBuilder.attclient_procedure_completed(
                 connection_handle, 0x0000, 0xFFFF))
 
     def stage_char_read_packets(
             self, att_handle, att_type, value, connection_handle=0x00):
         # Stage ble_rsp_attclient_read_by_handle (success)
-        self.backend._ser.stage_output(
+        self.mocked_serial.stage_output(
             BGAPIPacketBuilder.attclient_read_by_handle(
                 connection_handle, 0x0000))
         # Stage ble_evt_attclient_attribute_value
-        self.backend._ser.stage_output(
+        self.mocked_serial.stage_output(
             BGAPIPacketBuilder.attclient_attribute_value(
                 connection_handle, att_handle, att_type, [len(value)+1]+value))
 
     def stage_char_write_packets(
             self, handle, value, connection_handle=0x00):
         # Stage ble_rsp_attclient_attribute_write (success)
-        self.backend._ser.stage_output(
+        self.mocked_serial.stage_output(
             BGAPIPacketBuilder.attclient_attribute_write(
                 connection_handle, 0x0000))
         # Stage ble_evt_attclient_procedure_completed
-        self.backend._ser.stage_output(
+        self.mocked_serial.stage_output(
             BGAPIPacketBuilder.attclient_procedure_completed(
                 connection_handle, 0x0000, handle))
-
-    def stage_subscribe_packets(self, uuid_char, handle_char,
-                                indications=False, connection_handle=0x00):
-        # Stage get_handle packets
-        uuid_desc = '2902'
-        handle_desc = 0x5678
-        self.stage_get_handle_packets([
-            uuid_char, handle_char, uuid_desc, handle_desc])
-        handle = self.backend.get_handle(uuid_to_bytearray(uuid_char),
-                                         uuid_to_bytearray(uuid_desc))
-        # Stage char_write packets
-        if indications:
-            value = [0x02, 0x00]
-        else:
-            value = [0x01, 0x00]
-        self.stage_char_write_packets(handle, value,
-                                      connection_handle=connection_handle)
 
     def stage_indication_packets(
             self, handle, packet_values, connection_handle=0x00):
         # Stage ble_evt_attclient_attribute_value
         for value in packet_values:
             val = list(value)
-            self.backend._ser.stage_output(
+            self.mocked_serial.stage_output(
                 BGAPIPacketBuilder.attclient_attribute_value(
                     connection_handle, handle, 0x00, value=[len(val)+1]+val))
--- tests/bgapi/test_bgapi.py
+++ tests/bgapi/test_bgapi.py
@@ -1,16 +1,13 @@
 from __future__ import print_function
 
-from mock import patch
 from nose.tools import eq_, assert_in
-import platform
 import unittest
 import threading
 import time
 
 from pygatt.backends import BGAPIBackend
 
-from tests.serial_mock import SerialMock
-from .spy import BGAPIBackendSpy
+from .mocker import MockBGAPISerialDevice
 from .util import uuid_to_bytearray
 
 
@@ -19,144 +16,128 @@ class BGAPIBackendTests(unittest.TestCase):
     Test the functionality of the BGAPIBackend class.
     """
     def setUp(self):
-        self.patchers = []
-        patcher = patch('serial.Serial', return_value=SerialMock('dummy', 0.25))
-        patcher.start()
-        self.patchers.append(patcher)
-        # Where to write BGAPIBackend logfiles
-        system = platform.system()
-        self.null_file = '/dev/null'
-        if system.lower() == 'windows':
-            self.null_file = 'nul'
-
+        self.mock_device = MockBGAPISerialDevice()
         self.backend = BGAPIBackend(
-            serial_port='dummy', logfile=self.null_file, run=False)
-        self.spy = BGAPIBackendSpy(self.backend)
+            serial_port=self.mock_device.serial_port_name, run=False)
 
     def tearDown(self):
+        self.mock_device.stop()
         self.backend.stop()
-
-        for patcher in self.patchers:
-            try:
-                patcher.stop()
-            except RuntimeError:
-                pass
 
     def test_run_backend(self):
         """run general functionality."""
-        self.spy.stage_run_packets()
+        self.mock_device.stage_run_packets()
         self.backend.run()
 
     def test_connect(self):
         """connect general functionality."""
-        self.spy.stage_run_packets()
+        self.mock_device.stage_run_packets()
         self.backend.run()
         # Test connect
         address = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB]
-        self.spy.stage_connect_packets(
+        self.mock_device.stage_connect_packets(
             address, ['connected', 'completed'])
         self.backend.connect(bytearray(address))
 
     def test_disconnect_when_connected(self):
         """disconnect general functionality."""
-        self.spy.stage_run_packets()
+        self.mock_device.stage_run_packets()
         self.backend.run()
         address = [0x01, 0x23, 0x45, 0x67, 0x89, 0xab]
-        self.spy.stage_connect_packets(
+        self.mock_device.stage_connect_packets(
             address, ['connected', 'completed'])
         self.backend.connect(bytearray(address))
         # test disconnect (connected, not fail)
-        self.spy.stage_disconnect_packets(True, False)
+        self.mock_device.stage_disconnect_packets(True, False)
         self.backend.disconnect()
 
     def test_char_read(self):
         """read general functionality."""
-        self.spy.stage_run_packets()
+        self.mock_device.stage_run_packets()
         self.backend.run()
         address = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB]
-        self.spy.stage_connect_packets(
+        self.mock_device.stage_connect_packets(
             address, ['connected', 'completed'])
         self.backend.connect(bytearray(address))
         uuid_char = '01234567-0123-0123-0123-0123456789AB'
         handle_char = 0x1234
         uuid_desc = '2902'
         handle_desc = 0x5678
-        self.spy.stage_get_handle_packets([
+        self.mock_device.stage_get_handle_packets([
             uuid_char, handle_char,
             uuid_desc, handle_desc])
         handle = self.backend.get_handle(uuid_to_bytearray(uuid_char))
         # Test char_read
         expected_value = [0xBE, 0xEF, 0x15, 0xF0, 0x0D]
-        self.spy.stage_char_read_packets(
+        self.mock_device.stage_char_read_packets(
             handle, 0x00, expected_value)
         value = self.backend.char_read(handle)
         assert(value == bytearray(expected_value))
 
     def test_char_write(self):
         """char_write general functionality."""
-        self.spy.stage_run_packets()
+        self.mock_device.stage_run_packets()
         self.backend.run()
         address = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB]
-        self.spy.stage_connect_packets(
+        self.mock_device.stage_connect_packets(
             address, ['connected', 'completed'])
         self.backend.connect(bytearray(address))
         uuid_char = '01234567-0123-0123-0123-0123456789AB'
         handle_char = 0x1234
         uuid_desc = '2902'
         handle_desc = 0x5678
-        self.spy.stage_get_handle_packets([
+        self.mock_device.stage_get_handle_packets([
             uuid_char, handle_char,
             uuid_desc, handle_desc])
         handle = self.backend.get_handle(uuid_to_bytearray(uuid_char))
         # Test char_write
         value = [0xF0, 0x0F, 0x00]
-        self.spy.stage_char_write_packets(handle, value)
+        self.mock_device.stage_char_write_packets(handle, value)
         self.backend.char_write(handle, bytearray(value))
 
     def test_encrypt(self):
         """encrypt general functionality."""
-        self.spy.stage_run_packets()
+        self.mock_device.stage_run_packets()
         self.backend.run()
         address = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB]
-        self.spy.stage_connect_packets(
+        self.mock_device.stage_connect_packets(
             address, ['connected', 'completed'])
         self.backend.connect(bytearray(address))
         # Test encrypt
-        self.spy.stage_encrypt_packets(
+        self.mock_device.stage_encrypt_packets(
             address, ['connected', 'encrypted'])
         self.backend.encrypt()
 
     def test_bond(self):
         """bond general functionality."""
-        self.spy.stage_run_packets()
+        self.mock_device.stage_run_packets()
         self.backend.run()
         address = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB]
-        self.spy.stage_connect_packets(
+        self.mock_device.stage_connect_packets(
             address, ['connected', 'completed'])
         self.backend.connect(bytearray(address))
-        self.spy.stage_bond_packets(address,
-                                    ['connected', 'encrypted',
-                                     'parameters_change'])
+        self.mock_device.stage_bond_packets(
+            address, ['connected', 'encrypted', 'parameters_change'])
         self.backend.bond()
 
     def test_get_rssi(self):
         """get_rssi general functionality."""
-        self.spy.stage_run_packets()
+        self.mock_device.stage_run_packets()
         self.backend.run()
         address = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB]
-        self.spy.stage_connect_packets(
+        self.mock_device.stage_connect_packets(
             address, ['connected', 'completed'])
         self.backend.connect(bytearray(address))
         # Test get_rssi
-        self.spy.stage_get_rssi_packets()
+        self.mock_device.stage_get_rssi_packets()
         assert(self.backend.get_rssi() == -80)
 
     def test_get_handle(self):
         """get_handle general functionality."""
-        self.spy.stage_run_packets()
+        self.mock_device.stage_run_packets()
         self.backend.run()
         address = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB]
-        self.spy.stage_connect_packets(
+        self.mock_device.stage_connect_packets(
             address, ['connected', 'completed'])
         self.backend.connect(bytearray(address))
         # Test get_handle
@@ -164,7 +145,7 @@ class BGAPIBackendTests(unittest.TestCase):
         handle_char = 0x1234
         uuid_desc = '2902'
         handle_desc = 0x5678
-        self.spy.stage_get_handle_packets([
+        self.mock_device.stage_get_handle_packets([
             uuid_char, handle_char,
             uuid_desc, handle_desc])
         handle = self.backend.get_handle(uuid_to_bytearray(uuid_char))
@@ -175,7 +156,7 @@ class BGAPIBackendTests(unittest.TestCase):
 
     def test_scan_and_get_devices_discovered(self):
         """scan/get_devices_discovered general functionality."""
-        self.spy.stage_run_packets()
+        self.mock_device.stage_run_packets()
         self.backend.run()
         # Test scan
         scan_responses = []
@@ -190,12 +171,33 @@ class BGAPIBackendTests(unittest.TestCase):
             'data': [0x07, 0x09, ord('H'), ord('e'), ord('l'),
                      ord('l'), ord('o'), ord('!')]
         })
-        self.spy.stage_scan_packets(scan_responses=scan_responses)
+        self.mock_device.stage_scan_packets(scan_responses=scan_responses)
         self.backend.scan()
         devs = self.backend.get_devices_discovered()
         assert_in(addr_0_str, devs)
         eq_('Hello!', devs[addr_0_str].name)
         eq_(-80, devs[addr_0_str].rssi)
+
+    def stage_subscribe_packets(self, uuid_char, handle_char,
+                                indications=False, connection_handle=0x00):
+        # TODO this is a candidate to move to the BGAPIBackendSpy, but why does
+        # it need to call get_handle on the backend? otherwise it would just
+        # generate its own fake ouput for the serial device.
+
+        # Stage get_handle packets
+        uuid_desc = '2902'
+        handle_desc = 0x5678
+        self.mock_device.stage_get_handle_packets([
+            uuid_char, handle_char, uuid_desc, handle_desc])
+        handle = self.backend.get_handle(uuid_to_bytearray(uuid_char),
+                                         uuid_to_bytearray(uuid_desc))
+        # Stage char_write packets
+        if indications:
+            value = [0x02, 0x00]
+        else:
+            value = [0x01, 0x00]
+        self.mock_device.stage_char_write_packets(
+            handle, value, connection_handle=connection_handle)
 
     def test_subscribe_with_notify(self):
         """subscribe with notify general functionality."""
@@ -210,21 +212,22 @@ class BGAPIBackendTests(unittest.TestCase):
                 self.received_value_bytearray = received_value_bytearray
                 self.called.set()
 
-        self.spy.stage_run_packets()
+        self.mock_device.stage_run_packets()
         self.backend.run()
         address = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB]
-        self.spy.stage_connect_packets(address, ['connected', 'completed'])
+        self.mock_device.stage_connect_packets(
+            address, ['connected', 'completed'])
         self.backend.connect(bytearray(address))
         # Test subscribe with indications
         packet_values = [bytearray([0xF0, 0x0D, 0xBE, 0xEF])]
         my_handler = NotificationHandler(packet_values[0])
         handle = 0x1234
         uuid = '01234567-0123-0123-0123-0123456789AB'
-        self.spy.stage_subscribe_packets(uuid, handle)
+        self.stage_subscribe_packets(uuid, handle)
         self.backend.subscribe(uuid_to_bytearray(uuid),
                                callback=my_handler.handle, indicate=True)
         start_time = time.time()
-        self.spy.stage_indication_packets(handle, packet_values)
+        self.mock_device.stage_indication_packets(handle, packet_values)
         while not my_handler.called.is_set():
             elapsed_time = start_time - time.time()
             if elapsed_time >= 5:
@@ -237,18 +240,18 @@ class BGAPIBackendTests(unittest.TestCase):
 
     def test_delete_stored_bonds(self):
         """delete_stored_bonds general functionality."""
-        self.spy.stage_run_packets()
+        self.mock_device.stage_run_packets()
         self.backend.run()
         # Test delete stored bonds
-        self.spy.stage_delete_stored_bonds_packets(
+        self.mock_device.stage_delete_stored_bonds_packets(
             [0x00, 0x01, 0x02, 0x03, 0x04])
         self.backend.delete_stored_bonds()
 
     def test_delete_stored_bonds_disconnect(self):
         """delete_stored_bonds shouldn't abort if disconnected."""
-        self.spy.stage_run_packets()
+        self.mock_device.stage_run_packets()
         self.backend.run()
         # Test delete stored bonds
-        self.spy.stage_delete_stored_bonds_packets(
+        self.mock_device.stage_delete_stored_bonds_packets(
             [0x00, 0x01, 0x02, 0x03, 0x04], disconnects=True)
         self.backend.delete_stored_bonds()
