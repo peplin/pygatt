deaec905d582d4ef3ef5b9985d860dce60b7dadd
sloboste <sloboste@umich.edu>
2015-08-26 01:05:45 -0400

    Add get_rssi tests.

--- tests/test_device.py
+++ tests/test_device.py
@@ -7,7 +7,7 @@ from bluetooth_adapter import BleDevice
 
 class BleDeviceTest(unittest.TestCase):
 
-    mock_bgapi_backend = Mock()
+    mock_backend = Mock()
 
     def make_patch(self, *args, **kwargs):
         p = patch(*args, **kwargs)
@@ -22,19 +22,35 @@ class BleDeviceTest(unittest.TestCase):
             p.stop()
 
     def test_init(self):
-        ok_(BleDevice(Mock(), '01:23:45:67:89:AB', name='Hello, World!',
-            scan_response_rssi='-80'))
+        ok_(BleDevice(self.mock_backend, '01:23:45:67:89:AB',
+            name='Hello, World!', scan_response_rssi=-80))
 
     def test_get_name(self):
         expected_name = 'Hello, World!'
-        dev = BleDevice(Mock(), '01:23:45:67:89:AB', name=expected_name,
-                        scan_response_rssi='-80')
+        dev = BleDevice(self.mock_backend, '01:23:45:67:89:AB',
+                        name=expected_name, scan_response_rssi=-80)
         name = dev.get_name()
         eq_(name, expected_name)
 
     def test_get_mac_address(self):
         expected_mac = '01:23:45:67:89:AB'
-        dev = BleDevice(Mock(), expected_mac, name='Foobar',
-                        scan_response_rssi='-80')
+        dev = BleDevice(self.mock_backend, expected_mac, name='Foobar',
+                        scan_response_rssi=-80)
         mac = dev.get_mac_address()
         eq_(mac, expected_mac)
+
+    def test_get_rssi_from_scan_response(self):
+        expected_rssi = -74
+        dev = BleDevice(self.mock_backend, '01:23:45:67:89:AB', name='Foobar',
+                        scan_response_rssi=expected_rssi)
+        rssi = dev.get_rssi(from_connection=False)
+        eq_(rssi, expected_rssi)
+
+    def test_get_rssi_from_connection(self):
+        expected_rssi = -74
+        dev = BleDevice(self.mock_backend, '01:23:45:67:89:AB', name='Foobar',
+                        scan_response_rssi=-72)
+        dev.connect()
+        self.mock_backend.get_rssi.return_value = expected_rssi
+        rssi = dev.get_rssi(from_connection=True)
+        eq_(rssi, expected_rssi)
