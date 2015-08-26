2ecf22450bc4c995d7d19ae406e2ca0666557811
sloboste <sloboste@umich.edu>
2015-08-25 23:25:40 -0400

    Add list_bonds test.

--- tests/test_adapter.py
+++ tests/test_adapter.py
@@ -1,5 +1,5 @@
-from nose.tools import ok_
-from mock import patch
+from nose.tools import ok_, eq_
+from mock import Mock, patch
 import unittest
 
 from bluetooth_adapter import BluetoothAdapter
@@ -8,14 +8,18 @@ from bluetooth_adapter.backends import BackendEnum
 
 class BluetoothAdapterTest(unittest.TestCase):
 
-    def patch(self, *args, **kwargs):
+    EXAMPLE_BONDS = [0x00, 0x01, 0x02, 0x03, 0x04]
+    mock_bgapi_backend = Mock()
+
+    def make_patch(self, *args, **kwargs):
         p = patch(*args, **kwargs)
         p.start()
         self.patchers.append(p)
 
     def setUp(self):
         self.patchers = []
-        self.patch('bluetooth_adapter.adapter.BGAPIBackend')
+        self.make_patch('bluetooth_adapter.adapter.BGAPIBackend',
+                        return_value=self.mock_bgapi_backend)
 
     def tearDown(self):
         for p in self.patchers:
@@ -27,4 +31,12 @@ class BluetoothAdapterTest(unittest.TestCase):
     def test_enable_disable(self):
         adapter = BluetoothAdapter(BackendEnum.bgapi)
         adapter.enable()
+        adapter.disable()
+
+    def test_list_bonds(self):
+        self.mock_bgapi_backend.list_bonds.return_value = self.EXAMPLE_BONDS
+        adapter = BluetoothAdapter(BackendEnum.bgapi)
+        adapter.enable()
+        bonds = adapter.list_bonds()
+        eq_(bonds, self.EXAMPLE_BONDS)
         adapter.disable()
