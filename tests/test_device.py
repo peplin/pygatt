ed1d2be6356b85fbddb92a389720e2f44e7bb041
sloboste <sloboste@umich.edu>
2015-08-26 00:38:12 -0400

    Begin adding BleDevice tests.

---
+++ tests/test_device.py
@@ -0,0 +1,33 @@
+from nose.tools import ok_, eq_
+from mock import Mock, patch
+import unittest
+
+from bluetooth_adapter import BleDevice
+
+
+class BleDeviceTest(unittest.TestCase):
+
+    mock_bgapi_backend = Mock()
+
+    def make_patch(self, *args, **kwargs):
+        p = patch(*args, **kwargs)
+        p.start()
+        self.patchers.append(p)
+
+    def setUp(self):
+        self.patchers = []
+
+    def tearDown(self):
+        for p in self.patchers:
+            p.stop()
+
+    def test_init(self):
+        ok_(BleDevice(Mock(), '01:23:45:67:89:AB', name='Hello, World!',
+            scan_response_rssi='-80'))
+
+    def test_get_name(self):
+        expected_name = 'Hello, World!'
+        dev = BleDevice(Mock(), '01:23:45:67:89:AB', name=expected_name,
+                        scan_response_rssi='-80')
+        name = dev.get_name()
+        eq_(name, expected_name)
