053dae2f42a2be4bd9837d6c013dce8f0dd33159
Steven Davies <github@steev.me.uk>
2015-09-20 09:47:07 +0100

    Scan, connect and disconnect

--- pygatt/backends/dbusbackend/dbusbackend.py
+++ pygatt/backends/dbusbackend/dbusbackend.py
@@ -21,29 +21,38 @@ class DBusBackend(BLEBackend):
   '''
 
   def __init__(self, hci_device='hci0'):
+    gobject.threads_init()
+    dbus.mainloop.glib.threads_init()
+
     self._hci_device = hci_device
     self._adapter = None
     self._devices = {}
 
     self._dbus_loop = dbus.mainloop.glib.DBusGMainLoop(set_as_default = True)
-    dbus.mainloop.glib.threads_init()
     self._bus = dbus.SystemBus(mainloop = self._dbus_loop)
 
-    self._mainloop = None
+    self._mainloop = DbusBackendThread()
+
+  def start(self):
+    self._mainloop.start()
+
+  def stop(self):
+    self._mainloop.kill()
 
   def scan(self, timeout=10, run_as_root=False):
     manager = dbus.Interface(self._bus.get_object('org.bluez', '/'),
 				'org.freedesktop.DBus.ObjectManager')
     objects = manager.GetManagedObjects()
     for path, ifaces in objects.iteritems():
+      device = ifaces.get('org.bluez.Device1')
       adapter = ifaces.get('org.bluez.Adapter1')
-      if adapter is None:
-        continue
-      if self._hci_device == adapter['Address'] or \
-          path.endswith(self._hci_device):
-        obj = self._bus.get_object('org.bluez', path)
-        self._adapter = dbus.Interface(obj, 'org.bluez.Adapter1')
-        break
+      if adapter is not None:
+        if self._hci_device == adapter['Address'] or \
+            path.endswith(self._hci_device):
+          obj = self._bus.get_object('org.bluez', path)
+          self._adapter = dbus.Interface(obj, 'org.bluez.Adapter1')
+      elif device is not None:
+        self._add_device(path)
     if self._adapter is None:
       raise Exception('Bluetooth adapter not found')
 
@@ -55,19 +64,26 @@ class DBusBackend(BLEBackend):
       arg0 = 'org.bluez.Device1',
       path_keyword = 'path')
 
-    self._bus.add_signal_receiver(self._property_changed,
-      dbus_interface = 'org.bluez.Adapter1',
-      signal_name = 'PropertyChanged')
+    self._bus.add_signal_receiver(self._properties_changed,
+      signal_name = 'PropertiesChanged',
+      arg0 = 'org.bluez.Adapter1',
+      path_keyword = 'path')
 
     self._adapter.StartDiscovery()
-
-    gobject.threads_init()
-    self._mainloop = gobject.MainLoop()
-    gobject.timeout_add(timeout * 1000, self._mainloop.quit)
-    self._mainloop.run()
+    time.sleep(timeout)
     self._adapter.StopDiscovery()
 
     return [device for device in self._devices.values()]
+
+  def connect(self, address):
+    device = self._bus.get_object('org.bluez', self._devices[address]['path'])
+    device.Connect(dbus_interface='org.bluez.Device1')
+    log.debug('Connected to ' + self._devices[address]['path'])
+
+  def disconnect(self, address):
+    device = self._bus.get_object('org.bluez', self._devices[address]['path'])
+    device.Disconnect(dbus_interface='org.bluez.Device1')
+    log.debug('Disconnected from ' + self._devices[address]['path'])
 
   def _add_device(self, path):
     device = self._bus.get_object('org.bluez', path)
@@ -80,18 +96,32 @@ class DBusBackend(BLEBackend):
       log.info('Discovered %s: %s', address, name)
       self._devices[address] = {
         'address': address,
-        'name': name
+        'name': name,
+        'path': path
      }
 
   def _adapters_added(self, path, interfaces):
     self._add_device(path)
 
   def _properties_changed(self, interface, changed, invalidated, path):
+    if interface == 'org.bluez.Adapter1' and 'Discovering' in changed and changed['Discovering'] == False:
+      log.debug('Stop discovering')
+
     if interface != 'org.bluez.Device1':
       return
 
     self._add_device(path)
 
-  def _property_changed(name, value):
-    if (name == 'Discovering' and not value):
-      self._mainloop.quit()
+class DbusBackendThread(threading.Thread):
+  def __init__(self):
+    super(DbusBackendThread, self).__init__()
+    self._mainloop = gobject.MainLoop()
+    self._kill = False
+
+  def kill(self):
+    self._kill = True
+
+  def run(self):
+    context = self._mainloop.get_context()
+    while not self._kill:
+      context.iteration(True)
