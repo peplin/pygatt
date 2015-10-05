64202ca648236d1611286706eaad4479b8b9bc5d
Christopher Peplin <chris.peplin@rhubarbtech.com>
2015-10-05 14:33:42 -0400

    Remove unnecessary Adapter wrapper.

--- pygatt/backends/backend.py
+++ pygatt/backends/backend.py
@@ -1,5 +1,7 @@
 import logging
 
+from pygatt.constants import DEFAULT_CONNECT_TIMEOUT_S
+
 log = logging.getLogger(__name__)
 
 
@@ -19,7 +21,7 @@ class BLEBackend(object):
         """
         return True
 
-    def connect(self, address, **kwargs):
+    def connect(self, address, timeout=DEFAULT_CONNECT_TIMEOUT_S, **kwargs):
         """Return a BLEDevice for the connection if connected, otherwise raise
         an exception.
         """
@@ -32,6 +34,17 @@ class BLEBackend(object):
         Returns a list of BLE devices found.
         """
         raise NotImplementedError()
+
+    def filtered_scan(self, name_filter="", *args, **kwargs):
+        """
+        Scan for BLE devices and filter the list to include only with a name
+        that includes the given filter.
+
+        Returns a list of BLE devices found.
+        """
+        devices = self.scan(*args, **kwargs)
+        return [device for device in devices
+                if name_filter in (device['name'] or '')]
 
 
 class Characteristic(object):
