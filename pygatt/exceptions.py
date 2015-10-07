7efcb1a68e2d7602933bd839344f2e0bd4361fd4
Christopher Peplin <chris.peplin@rhubarbtech.com>
2015-10-07 14:28:30 -0400

    Render NotConnected exception as a proper string.

--- pygatt/exceptions.py
+++ pygatt/exceptions.py
@@ -8,6 +8,9 @@ class BLEError(Exception):
     def __repr__(self):
         return "<%s %s>" % (self.__class__.__name__, self.message)
 
+    def __str__(self):
+        return repr(self)
+
 
 class NotConnectedError(BLEError):
     pass
