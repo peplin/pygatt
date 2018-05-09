2f7158b288aef5d10e741ab907eb4a8897bd53b2
dcliftreaves <david.cliftreaves@gmail.com>
2018-05-09 12:51:27 -0500

    2 minutes before we give up on bad devices
    That settled on 2 minutes as a good amount of time before giving up.

--- pygatt/backends/bluez/device.py
+++ pygatt/backends/bluez/device.py
@@ -47,7 +47,7 @@ class BluezBLEDevice(BLEDevice):
         # created us to allow orselves to be removed by the correct adapter.
         self._backend = _backend
         self._is_troublesome = is_troublesome
-        self._last_time_connected = None
+        self._last_time_connected = time.time()
 
     def subscribe(self, uuid, callback=None, indication=False):
         global g_prop_changed
@@ -192,7 +192,7 @@ class BluezBLEDevice(BLEDevice):
                     if self._is_troublesome == True and is_connect == True:
                         # After much hand wringing I have found that time is the only way to know
                         # that there is a problem.
-                        if self._last_time_connected != None and time.time() > (self._last_time_connected + (0*60)):
+                        if time.time() > (self._last_time_connected + (2*60)):
                             import sys; sys.exit(36)
                     pass
 
